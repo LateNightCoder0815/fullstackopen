6465bba2499921d976fa9392a7c9b19c66f4ecee
sven <shuesche@yahoo.de>
2020-09-03 15:49:23 +0200

    7.14

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import Blog from './components/Blog'
+import User from './components/User'
+import IndividualUser from './components/IndividualUser'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
 import Togglable from './components/Togglable'
@@ -7,15 +9,21 @@ import Notification from './components/Notification'
 import blogService from './services/blogs'
 import { useDispatch, useSelector } from 'react-redux'
 import { initBlog } from './reducers/blogReducer'
+import { initUsers } from './reducers/usersReducer'
 import { setUser } from './reducers/userReducer'
+import {
+  Switch, Route, Link, useRouteMatch, useHistory
+} from "react-router-dom"
 
 const App = () => {
   const dispatch = useDispatch()
   const blogs = useSelector(state => state.blogs)
   const user = useSelector(state => state.user)
-  const blogFormRef = useRef()
+  const users = useSelector(state => state.users)
 
+  const blogFormRef = useRef()
   useEffect(() => { dispatch(initBlog())}, [dispatch])
+  useEffect(() => { dispatch(initUsers())}, [dispatch])
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
     if (loggedUserJSON) {
@@ -30,6 +38,9 @@ const App = () => {
     dispatch(setUser(null))
   }
 
+  const match = useRouteMatch('/users/:id')
+  const selectedUser = match ? users.find(n => n.id === match.params.id) : null
+
   return (
     <div>
       <Notification />
@@ -39,12 +50,24 @@ const App = () => {
         <div>
           <h1>Blogs</h1>
           <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
-          <Togglable buttonLabel='new blog' ref={blogFormRef}>
-            <h2>Create new</h2>
-            <BlogForm blogFormRef={blogFormRef}/>
-          </Togglable>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} user={user}/>)}
+          <Switch>
+          <Route path='/users/:id'>
+            <IndividualUser user={selectedUser} />
+          </Route> 
+          <Route path='/users'>
+            <h1>Users</h1>
+            {users.map(user =>
+              <User key={user.id} user={user} />)}
+          </Route> 
+            <Route path='/'>
+              <Togglable buttonLabel='new blog' ref={blogFormRef}>
+                <h2>Create new</h2>
+                <BlogForm blogFormRef={blogFormRef}/>
+              </Togglable>
+              {blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} user={user}/>)}
+            </Route>
+          </Switch>
         </div>
       }
     </div>
---
+++ part7/bloglist-frontend/src/components/IndividualUser.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const IndividualUser = ({ user }) => {
+if (!user) {
+  return null
+}
+return(
+  <div>
+    <h1>{user.name}</h1>
+    <h2>added blogs</h2>
+    <ul>
+      {user.blogs.map(blog => 
+        <li key={blog.id}>{blog.title}</li>)}
+    </ul>
+  </div>
+)}
+
+export default IndividualUser
